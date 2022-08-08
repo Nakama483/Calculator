467a43ebb5d22af3278dc95182211e7b23e87f05
Alex <xalexgs34@gmail.com>
2022-08-08 14:13:58 -0700

    Add storing operands in variables

--- javascript.js
+++ javascript.js
@@ -47,6 +47,11 @@ const divide = document.getElementById('divide');
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 
+let x;
+let y;
+let operator;
+const reg = /[x+-/](.*)/;
+
 one.addEventListener('click', () => display.textContent += 1);
 two.addEventListener('click', () => display.textContent += 2);
 three.addEventListener('click', () => display.textContent += 3);
@@ -58,9 +63,40 @@ eight.addEventListener('click', () => display.textContent += 8);
 nine.addEventListener('click', () => display.textContent += 9);
 zero.addEventListener('click', () => display.textContent += 0);
 
-add.addEventListener('click', () => display.textContent += '+');
-subtract.addEventListener('click', () => display.textContent += '-');
-multiply.addEventListener('click', () => display.textContent += 'x');
-divide.addEventListener('click', () => display.textContent += '/');
+
+add.addEventListener('click', () => display.textContent += ' + ');
+subtract.addEventListener('click', () => display.textContent += ' - ');
+multiply.addEventListener('click', () => display.textContent += ' x ');
+divide.addEventListener('click', () => display.textContent += ' / ');
+
+add.addEventListener('click', () => x = +display.textContent.slice(0, -2));
+subtract.addEventListener('click', () => x = +display.textContent.slice(0, -2));
+multiply.addEventListener('click', () => x = +display.textContent.slice(0, -2));
+divide.addEventListener('click', () => x = +display.textContent.slice(0, -2));
+
+equals.addEventListener('click', () => y = display.textContent.match(reg)[1]);
+equals.addEventListener('click', () => console.log(y));
 equals.addEventListener('click', () => display.textContent = operate());
 clear.addEventListener('click', () => display.textContent = '');
+
+
+
+
+
+
+
+
+
+
+
+// one.addEventListener('click', () => x = 1);
+// one.addEventListener('click', () => console.log(x));
+// two.addEventListener('click', () => x = 2);
+// three.addEventListener('click', () => x = 3);
+// four.addEventListener('click', () => x = 4);
+// five.addEventListener('click', () => x = 5);
+// six.addEventListener('click', () => x = 6);
+// seven.addEventListener('click', () => x = 7);
+// eight.addEventListener('click', () => x = 8);
+// nine.addEventListener('click', () => x = 9);
+// zero.addEventListener('click', () => x = 0);
